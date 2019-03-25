1b3c5f353b0ee27d6d19e14debabe772f5892a5a
DreamBoy <xiao@XIAOdeMacBook-Pro.local>
2019-03-26 07:16:12 +0800

    Initial Commit

---
+++ LXScrollLabel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LXScrollLabel
+//
+//  Created by XIAO on 2019/3/26.
+//  Copyright © 2019年 XIAO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LXScrollLabel/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  LXScrollLabel
+//
+//  Created by XIAO on 2019/3/26.
+//  Copyright © 2019年 XIAO. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
