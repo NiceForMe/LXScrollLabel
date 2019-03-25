1976a44e3618625e694aeb22066248064a7b9b97
NiceForMe <771717844@qq.com>
2019-03-26 07:37:22 +0800

    commit again

--- LXScrollLabel/ViewController.m
+++ LXScrollLabel/ViewController.m
@@ -34,6 +34,7 @@
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(5.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         label.timeInterval = 1.0;
     });
+    
 }
 
 - (NSInteger)numberOfRowsInScrollLabel:(LXScrollLabel *)scrollLabel
